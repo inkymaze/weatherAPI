37022cb2679d9a87d342524e2841271db91f2f72
inkymaze <nickwcannon@gmail.com>
2017-08-31 12:44:38 -0700

    add google api

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,7 +5,8 @@ export default function(state = [], action) {
 
   switch(action.type){
     case FETCH_WEATHER:
-      // two lines below do the same thing 
+      // two lines below do the same thing
+      
       // return state.concat([ action.payload.data ]);
       return [ action.payload.data, ...state];
   }
